8f6bbc075dfc55b5bd53704ef59eca053438f607
itu-itis21-gursu20 <gursu20@itu.edu.tr>
2023-01-20 10:34:59 +0300

    ödev 1 cevapları yüklendi

--- patika_odev1.sql
+++ patika_odev1.sql
@@ -1,6 +1,6 @@
 -- ÖDEV 1 
 
--- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veritabanı üzerinden gerçekleştiriniz.
 
 -- 1- film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.  
 	  SELECT title, description FROM film;
